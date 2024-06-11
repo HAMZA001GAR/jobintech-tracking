85c15f5ff9f91e1255450d42d23ded77a57980d6
ZinebELMALKI <elmalki.z.fst@uhp.ac.ma>
2024-06-11 19:22:09 +0100

    services 2

--- src/main/java/com/jobintechtracking/app/services/DoingService.java
+++ src/main/java/com/jobintechtracking/app/services/DoingService.java
@@ -1,4 +1,28 @@
 package com.jobintechtracking.app.services;
 
+import com.jobintechtracking.app.entities.Doing;
+import com.jobintechtracking.app.entities.Formation;
+import com.jobintechtracking.app.repositories.DoingRepository;
+
+import java.util.List;
+
 public class DoingService {
+
+    DoingRepository doingRepository;
+    public DoingService(DoingRepository doingRepository) {
+        this.doingRepository = doingRepository;
+    }
+
+    public Doing save(Doing doing) {
+        return doingRepository.save(doing);
+    }
+
+
+    public Doing findById(Long id) {
+        return  doingRepository.findById(id).orElse(null);
+    }
+
+    public List<Doing> findAll(){
+        return doingRepository.findAll();
+    }
 }
