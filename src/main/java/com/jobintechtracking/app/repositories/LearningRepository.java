7adcbae509aae4895b6f11c98621453e7b950250
Nadia Outizi <107075774+NadiaOutizi@users.noreply.github.com>
2024-06-11 16:07:25 +0100

    n1

--- src/main/java/com/jobintechtracking/app/repositories/LearningRepository.java
+++ src/main/java/com/jobintechtracking/app/repositories/LearningRepository.java
@@ -1,6 +1,7 @@
 package com.jobintechtracking.app.repositories;
 
+import com.jobintechtracking.app.entities.Learning;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LearningRepository extends JpaRepository {
+public interface LearningRepository extends JpaRepository<Learning,Long> {
 }
--- src/main/java/com/jobintechtracking/app/repositories/StepRepository.java
+++ src/main/java/com/jobintechtracking/app/repositories/StepRepository.java
@@ -1,6 +1,7 @@
 package com.jobintechtracking.app.repositories;
 
+import com.jobintechtracking.app.entities.Steps;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StepRepository extends JpaRepository {
+public interface StepRepository extends JpaRepository<Steps,Long> {
 }
